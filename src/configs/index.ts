79ad99faa1923ef25d4402da78f1235cce2b4242
Pallab Bharman <bharmanpallab@gmail.com>
2025-11-09 12:50:55 +0600

    prisma added

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -6,4 +6,5 @@ dotenv.config({ path: path.join(process.cwd(), '.env') });
 export default {
     env: process.env.NODE_ENV,
     port: process.env.PORT,
+    databaseUrl: process.env.DATABASE_URL,
 };
